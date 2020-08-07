28353a61d63f34f8f38287d02403a19075b11be1
zlf <2878253979@qq.com>
2020-08-07 14:42:58 +0800

    贡献榜排行

--- pages/subPages/user/rank/rank.vue
+++ pages/subPages/user/rank/rank.vue
@@ -1,16 +1,60 @@
 <template>
 	<view class="container">
-		<!-- 列表 -->
+		<view class='tab-container'>
+			<view class="tab">
+				<view class="tab-item" :class='{active:current==0}' @tap='switchAct(0)'>周榜</view>
+				<view class="tab-item" :class='{active:current==1}' @tap='switchAct(1)'>月榜</view>
+				<view class="tab-item" :class='{active:current==2}' @tap='switchAct(2)'>总榜</view>
+			</view>
+		
+		</view>
+		
+		<view class="top-three">
+			<view class="top-three-img">
+				<view class="two">
+					<image style="width: 141rpx; height: 149rpx; z-index: 5;" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HYKTMib185fwujoD0wb2EuDGjh9MawJOZ61J0ykJHzKrksSf1zBfVFHCictohBCicA4JWwNC5YljiaSQ/0" mode="aspectFill"></image>
+					<image class="user_avatar" :src="userRank[1].avatar || AVATAR" mode="aspectFill"></image>
+				</view>
+				<view class="one">
+					<image style="width: 184rpx; height: 194rpx; z-index: 5;" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HYKTMib185fwujoD0wb2EuDCEt2u0s0qbuu6RF8k8252ttLQyWmf5XFfH5jV8F5Gc4857pPXkR5Tw/0" mode="aspectFill"></image>
+					<image class="user_avatar" :src="userRank[0].avatar || AVATAR" mode="aspectFill"></image>
+				</view>
+				<view class="three">
+					<image style="width: 128rpx; height: 135rpx; z-index: 5;" src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HYKTMib185fwujoD0wb2EuDONRAdg8QLbTibHjChAgSQicUh6H037akJyQAicnDGvBK7DHGWrniapU4Bg/0" mode="aspectFill"></image>
+					<image class="user_avatar" :src="userRank[2].avatar || AVATAR" mode="aspectFill"></image>
+				</view>
+				
+			</view>
+			<view class="top-three-bg">
+				<view class="top-three-name">
+					<view class="two-name">
+						<view class="name">{{userRank[1].nickname || NICKNAME}}</view>
+						<view class="hot">{{userRank[1].hot || 0}}</view>
+					</view>
+					<view class="one-name">
+						<view class="name">{{userRank[0].nickname || NICKNAME}}</view>
+						<view class="hot">{{userRank[0].hot || 0}}</view>
+					</view>
+					<view class="three-name">
+						<view class="name">{{userRank[2].nickname || NICKNAME}}</view>
+						<view class="hot">{{userRank[2].hot || 0}}</view>
+					</view>
+				</view>
+				<image src="https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9HYKTMib185fwujoD0wb2EuDDuXPbPbxY7bF3JR1HoIEWiakdicq8qkXDm02jwxkwD2td0prT2PnSBCw/0" mode="widthFix"></image>
+				
+			</view>
+		</view>
+		
 		<view class="list-wrapper">
-			<view class="item" :class="{one:index==0,two:index==1,three:index==2}" v-for="(item,index) in userRank" :key="index">
+			<view class="item" v-for="(item,index) in userRank" :key="index" v-if="index>2">
 				<view class="rank-num">
 					<view>{{index+1}}</view>
 				</view>
 				<view class='avatar'>
-					<image :src="item.avatar" mode="aspectFill"></image>
+					<image :src="item.avatar || AVATAR" mode="aspectFill"></image>
 				</view>
 				<view class="text-container">
-					<view class="star-name text-overflow">{{item.nickname}}</view>
+					<view class="star-name text-overflow">{{item.nickname || NICKNAME}}</view>
 
 				</view>
 				<!-- <view class="level">lv10</view> -->
@@ -18,7 +62,6 @@
 			</view>
 
 		</view>
-		<!-- 我的 -->
 		<view class="my-wrap" v-if="$app.getData('userStar').id == starid">
 			<view class="rank-num">
 				<view>{{myInfo.rank}}</view>
@@ -41,7 +84,9 @@
 		data() {
 			return {
 				requestCount: 1,
-
+				current: 0,
+				AVATAR: this.$app.getData('AVATAR'),
+				NICKNAME: this.$app.getData('NICKNAME'),
 				starid: null,
 				userRank: [],
 				page: 1,
@@ -58,10 +103,25 @@
 			}
 		},
 		methods: {
+			switchAct(current) {
+				this.page = 1
+				this.current = current
+				this.loadData()
+			},
 			loadData() {
+				if (this.page > 10) return
+				let field;
+				if (this.current == 0) {
+					field = 'thisweek_count'
+				} else if (this.current == 1) {
+					field = 'thismonth_count'
+				} else if (this.current == 2) {
+					field = 'total_count'
+				}
 				this.$app.request(this.$app.API.USER_RANK, {
 					starid: this.starid,
-					page: this.page
+					page: this.page,
+					field,
 				}, res => {
 					this.myInfo = res.data.my
 					const resList = []
@@ -69,7 +129,7 @@
 						resList.push({
 							avatar: e.user && e.user.avatarurl || this.$app.getData('AVATAR'),
 							nickname: e.user && e.user.nickname || this.$app.getData('NICKNAME'),
-							hot: this.$app.formatNumberRgx(e.thisweek_count),
+							hot: this.$app.formatNumberRgx(e.hot),
 						})
 					})
 
@@ -86,23 +146,171 @@
 
 <style lang="scss" scoped>
 	.container {
-		.list-wrapper {
-
-			.item {
-				margin: 20upx 0;
-				height: 130upx;
-				background: url(https://mmbiz.qpic.cn/mmbiz_png/w5pLFvdua9Hf1Erx2q066sKAsgEzqUsxtR4IyxJscPr234VZVlMPb9Z2TSwvzWYBOMtribqMrLGIUAbJGtyERqA/0) right center no-repeat/contain;
+		.tab-container {
+			width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			padding-top: 40rpx;
+		
+			.tab {
+				width: 100%;
+				height: 80rpx;
+				color: #FFFFFF;
 				display: flex;
 				align-items: center;
+		
+				.tab-item {
+					width: 200rpx;
+					padding: 15upx 0upx;
+					margin: 0 40rpx;
+					background:linear-gradient(0deg,rgba(208,208,208,1) 0%,rgba(175,175,175,1) 100%);
+					justify-content: center;
+					display: flex;
+					font-size: 28upx;
+					flex: 1;
+					border-radius: 60rpx;
+				}
+		
+				.tab-item.active {
+					background:linear-gradient(to right bottom, #FF3A8A 20%, #fa6c9f 82%, #ffe140 100%) !important;
+					text-align: center;
+				}
+			}
+		
+		}
+		
+		.top-three{
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			padding-top: 30rpx;
+			
+			.top-three-img{
+				width: 90%;
+				display: flex;
+				padding-bottom: 20rpx;
+				z-index: 5;
+				view{
+					width: 33%;
+					display: flex;
+					justify-content: center;
+				}
+				.one {
+					position: relative;
+					.user_avatar{
+						width: 151rpx;
+						height: 151rpx;
+						position: absolute;
+						border-radius: 50%;
+						top: 37rpx;
+						left: 46rpx;
+						z-index: 4;
+
+					}
+				}
+				
+				.two {
+					margin-top: 110rpx;
+					position: relative;
+					.user_avatar{
+						width: 112rpx;
+						height: 112rpx;
+						position: absolute;
+						border-radius: 50%;
+						top: 34rpx;
+						left: 68rpx;
+						z-index: 4;
+					}
+				}
+				
+				.three {
+					margin-top: 140rpx;
+					position: relative;
+					.user_avatar{
+						width: 104rpx;
+						height: 104rpx;
+						position: absolute;
+						border-radius: 50%;
+						top: 28rpx;
+						left: 68rpx;
+						z-index: 4;
+
+					}
+				}
+			}
+			.top-three-bg{
+				width: 90%;
+				z-index: 1;
+				display: flex;
+				flex-direction: column;
+				image{
+					margin-top: -200rpx;
+				}
+			
+				.top-three-name{
+					display: flex;
+					flex-direction: row;
+					color: #FFFFFF;
+					padding-top: 20rpx;
+					z-index: 1;
+					
+					view{
+						width: 33%;
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+						text-align: center;
+						.name{
+							width: 100%;
+							font-size: 32rpx;
+							padding-bottom: 10rpx;
+							text-overflow: ellipsis;
+							white-space: nowrap;
+							overflow: hidden;
+						}
+						.hot{
+							width: 100%;
+						}
+					}
+					.one-name{
+						margin-top: -120rpx;
+					}
+					
+				}
+			}
+		}
+		
+		.list-wrapper {
+			display: flex;
+			flex-direction: column;
+			background: #FFFFFF;
+			border-radius: 20rpx;
+			padding: 0 20rpx;
+			z-index: 9;
+			margin-top: -30rpx;
+			margin-bottom: 150rpx;
+
+			.item {
+				height: 130upx;
+				background: #FFFFFF;
+				border-bottom: 2rpx solid #F3F3F3;
+				display: flex;
+				align-items: center;
+				color: #4D2D2D;
+				font-weight: bold;
+				z-index: 9;
 
 				.rank-num {
-					margin-left: 106upx;
+					margin-left: 30upx;
+					
 				}
 
 				.avatar image {
 					margin-left: 60upx;
-					width: 100upx;
-					height: 100upx;
+					width: 80upx;
+					height: 80upx;
 					border-radius: 50%;
 				}
 
@@ -110,6 +318,12 @@
 					margin-left: 30upx;
 					width: 250upx;
 					line-height: 44upx;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					overflow: hidden;
+					
+					.star-name{
+					}
 
 					.bottom-text {
 						display: flex;
@@ -123,23 +337,16 @@
 				}
 
 				.count {
-					margin-left: 30upx;
+					padding-right: 30upx;
+					flex: 1 0%;
+					text-align: right;
+					color: #E99990;
 				}
 
 
 			}
 
-			.item.one {
-				background: url(http://tva1.sinaimg.cn/large/0060lm7Tly1g2enrl4pe1g30j703n0sk.gif) right center no-repeat/contain;
-			}
-
-			.item.two {
-				background: url(http://tva1.sinaimg.cn/large/0060lm7Tly1g2enrl5aerg30j703ndfn.gif) right center no-repeat/contain;
-			}
-
-			.item.three {
-				background: url(http://tva1.sinaimg.cn/large/0060lm7Tly1g2enrl4zd6g30j703ngle.gif) right center no-repeat/contain;
-			}
+			
 		}
 
 		.my-wrap {
@@ -149,17 +356,20 @@
 			margin: 20upx 0;
 			height: 130upx;
 			display: flex;
-			background-color: $text-color-1;
+			background-color: #EFCCC8;
 			align-items: center;
+			font-weight: bold;
+			z-index: 99;
 
 			.rank-num {
-				margin-left: 106upx;
+				margin-left: 30upx;
+				
 			}
 
 			.avatar image {
 				margin-left: 60upx;
-				width: 100upx;
-				height: 100upx;
+				width: 80upx;
+				height: 80upx;
 				border-radius: 50%;
 			}
 
@@ -167,20 +377,29 @@
 				margin-left: 30upx;
 				width: 250upx;
 				line-height: 44upx;
-
+				text-overflow: ellipsis;
+				white-space: nowrap;
+				overflow: hidden;
+				
+				.star-name{
+				}
+			
 				.bottom-text {
 					display: flex;
 					align-items: center;
 					color: $color_2;
 				}
 			}
+			
 
 			.level {
 				// margin-left: 30upx;
 			}
 
 			.count {
-				margin-left: 30upx;
+				padding-right: 30upx;
+				flex: 1 0%;
+				text-align: right;
 			}
 
 		}
